cea8aad3d935903bb13e23d02266cc241a26edfa
Caa2050 <65472230+Caa2050@users.noreply.github.com>
2021-12-09 22:25:10 -0300

    Add files via upload

---
+++ estrutura_pagina/footer.php
@@ -0,0 +1,35 @@
+<!--JavaScript at end of body for optimized loading-->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+      <script>
+      	M.AutoInit();
+      </script>
+      <footer class="page-footer grey darken-3 ">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Sobre o site</h5>
+                <p class="grey-text text-lighten-4">O site tem como o objetivo ajudar alunos a encontrarem vagas de estagio e emprego proximos a eles, satisfazendo os requisitos necessarios para vaga e tendo tempo disponivel para a realização da mesma.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Linkedin dos desenvolvedores</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/in/carlos-augusto-amaro-2414801b7?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B%2B%2BAXW7r1SBmrAwZly892cw%3D%3D">Carlos Augusto Amaro</a></li>
+                  <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/in/marieta-marreira-1096ba1a2?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3Bm53hnIkkSvudBNoBBVOF2Q%3D%3D">Marieta Marreira</a></li>
+                  <li><a class="grey-text text-lighten-3" href="https://www.linkedin.com/in/marilza-cristina-85a2021a5?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3B3h3EqJaMT7ClO1NoRoNBVg%3D%3D">Marilza Cristina</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2021 Search Vagas. Todos os direitos reservados.
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
+     </main>
+    </body>
+  </html>
+  <?php
+  // o footer.php é parte final da pagina, onde coloca demais links e copyright
+  ?>
---
+++ estrutura_pagina/menu2.php
@@ -0,0 +1,12 @@
+<nav class="nav-extended grey darken-3">
+    <div class="nav-wrapper">
+    	<a href="#" class="brand-logo center">Search Vagas</a>
+    </div>
+    <div class="nav-content red accent-4">
+      <ul class="tabs tabs-transparent">
+     	<li class="tab"><a href="#aluno">Para Alunos</a></li>
+    	 <li class="tab"><a href="#recrutador">Para Recrutadores</a></li>
+  		</ul>
+      </ul>
+    </div>
+ </nav>
