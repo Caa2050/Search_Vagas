0b30f3662c560f100e7bc68269dd6445c38e938f
Caa2050 <65472230+Caa2050@users.noreply.github.com>
2021-12-09 22:23:05 -0300

    Add files via upload

--- conexão/db_connect.php
+++ conexão/db_connect.php
@@ -1,1 +1,14 @@
+<?php
+function conectamy($host, $user, $passwd, $dbase)
+{ 
+  global $nulink;
+  $nulink=mysqli_connect($host,$user,$passwd,$dbase);
 
+  mysqli_query($nulink,"SET NAMES 'utf8'");
+  mysqli_query($nulink,'SET character_set_connection=utf8');
+  mysqli_query($nulink,'SET character_set_client=utf8');
+  mysqli_query($nulink,'SET character_set_results=utf8');
+}
+conectamy("127.0.0.1:52527","azure","6#vWHD_$","bancotg");
+
+?>
